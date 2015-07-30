1a6f00798c12a3a20497d80b47d3f137c9fd4433
Konemiees <konsta.sinisalo@gmail.com>
2015-07-30 18:17:58 +0300

    oikeesti hyvä kääntymine ja ookoo kamera

--- Underdog 2/Assets/Scripts/CameraFollow.cs
+++ Underdog 2/Assets/Scripts/CameraFollow.cs
@@ -18,21 +18,30 @@ public class CameraFollow : MonoBehaviour
 	public float cameraUpAngleLimit = 58;
 	public float cameraCloseLimit = 1;
 	private float lowLimitAngle;
+
 	private Transform foundTarget;
+
+	private Camera cam;
 
 	//@script AddComponentMenu("Camera-Control/Mouse Orbit");
 	
 	void Start ()
 	{
+		Cursor.visible = false;
 		x = transform.eulerAngles.y; 
 		y = transform.eulerAngles.x;
 	
 		// Calculate the initial offset.
 		offset = transform.position - target.position;
+
+		cam = Camera.main;
 	}
 	
 	void FixedUpdate ()
 	{
+
+
+		checkTarget ();
 		 
 		 if (target && !foundTarget) { 
 			x += Input.GetAxis("Mouse X") * xSpeed * distance; //0.02f; 
@@ -92,6 +101,39 @@ public class CameraFollow : MonoBehaviour
 
 	}
 
+
+
+	void checkTarget(){
+
+		if (!foundTarget) {
+			for (int i = 2; i > -3; i--) {
+
+				Ray ray;
+				RaycastHit hit;
+
+				//Horizontal raycast
+				ray = cam.ScreenPointToRay (new Vector3 (cam.pixelWidth / 2 + (i * .5f), cam.pixelHeight / 2, 0));
+
+				if (Physics.Raycast (ray, out hit)) {
+					if (hit.transform.gameObject.tag == "Targetable" && Input.GetButtonDown ("Lock"))
+						foundTarget = hit.transform;
+				}
+
+				//Vertical raycast
+				ray = cam.ScreenPointToRay (new Vector3 (cam.pixelWidth / 2, cam.pixelHeight / 2 + (i * .5f), 0));
+
+				if (Physics.Raycast (ray, out hit)) {
+					if (hit.transform.gameObject.tag == "Targetable" && Input.GetButtonDown ("Lock"))
+						foundTarget = hit.transform;
+				}
+
+			}
+		} else {
+			if (Input.GetButtonDown ("Lock"))
+				foundTarget = null;
+		}
+	
+		}
 
 
 
